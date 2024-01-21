52abaec4b34d4c2a483c1ba00d5235e3f83f4212
Color-Kat <katik7979@gmail.com>
2024-01-21 21:57:00 +0300

    auto update feed when create new tweet

--- src/app/components/NewTweetForm.tsx
+++ src/app/components/NewTweetForm.tsx
@@ -1,22 +1,23 @@
 'use client';
 
-import React, {memo, FC, useEffect, useRef, useLayoutEffect, useCallback} from 'react';
-import {Button} from "@/app/UI/Button";
-import {ProfileImage} from "@/app/components/ProfileImage";
-import {useSession} from "next-auth/react";
-import {api} from "@/trpc/react";
+import React, { memo, FC, useEffect, useRef, useLayoutEffect, useCallback } from 'react';
+import { Button } from "@/app/UI/Button";
+import { ProfileImage } from "@/app/components/ProfileImage";
+import { useSession } from "next-auth/react";
+import { api } from "@/trpc/react";
+import { Tweet } from "@/app/types/Tweet";
 
 function updateTextAreaSize(textarea?: HTMLTextAreaElement) {
-    if(!textarea) return;
+    if (!textarea) return;
     textarea.style.height = "0";
     textarea.style.height = `${textarea.scrollHeight}px`;
 }
 
 export const NewTweetForm: FC = memo(({}) => {
     const session = useSession();
-    if(session.status !== "authenticated") return null;
+    if (session.status !== "authenticated") return null;
 
-    return <Form />;
+    return <Form/>;
 });
 
 
@@ -36,9 +37,42 @@ const Form: FC = ({}) => {
         updateTextAreaSize(textAreaRef.current);
     }, [inputValue]);
 
+    const trpcUtils = api.useContext()
+
     const createTweet = api.tweet.create.useMutation({
         onSuccess: newTweet => {
-            setInputValue("")
+            setInputValue("");
+
+            if(session.status !== "authenticated") return;
+
+            trpcUtils.tweet.infiniteFeed.setInfiniteData({}, (oldData) => {
+                if(oldData == null || oldData.pages[0] == null) return;
+
+                const newCacheTweet = {
+                    ...newTweet,
+                    likeCount: 0,
+                    likedByMe: false,
+                    user: {
+                        id: session.data.user.id,
+                        name: session.data.user.name,
+                        image: session.data.user.image
+                    }
+                } as Tweet;
+
+                return {
+                    ...oldData,
+                    pages: [
+                        {
+                            ...oldData.pages[0],
+                            tweets: [
+                                newCacheTweet,
+                                ...oldData.pages[0].tweets
+                            ]
+                        },
+                        ...oldData.pages.slice(1)
+                    ]
+                }
+            });
         }
     });
     const handleSubmit = (e: React.FormEvent) => {
@@ -49,16 +83,16 @@ const Form: FC = ({}) => {
         });
     }
 
-    if(session.status !== "authenticated") return null;
+    if (session.status !== "authenticated") return null;
 
     return (
         <form onSubmit={handleSubmit} className="flex flex-col gap-2 border-b px-4 py-2">
             <div className="flex gap-4">
-                <ProfileImage src={session.data.user.image} />
+                <ProfileImage src={session.data.user.image}/>
 
                 <textarea
                     ref={inputRef}
-                    style={{height: 0}}
+                    style={{ height: 0 }}
                     value={inputValue}
                     onChange={(e) => setInputValue(e.target.value)}
                     className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
