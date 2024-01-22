82f60efa396613d60ae99780c56c55e0bfad1cef
Color-Kat <katik7979@gmail.com>
2024-01-22 21:19:11 +0300

    loader

---
+++ src/app/components/LoadingSpinner.tsx
@@ -0,0 +1,16 @@
+import React, {memo, FC} from 'react';
+import { VscRefresh } from "react-icons/vsc";
+import { twJoin } from "tailwind-merge";
+
+export const LoadingSpinner: FC<{big?: boolean}> = memo(({big = false}) => {
+    const sizeClasses = big ? "w-16 h-16" : "w-10 h-10";
+    
+    return (
+        <div className="flex justify-center p-2">
+            <VscRefresh className={twJoin(
+                sizeClasses,
+                "animate-spin"
+            )}/>
+        </div>
+    );
+});
