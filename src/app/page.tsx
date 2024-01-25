4ee5a0e6d20356e3d71f955c1d3e6c13d90a0002
Color-Kat <katik7979@gmail.com>
2024-01-25 21:20:09 +0300

    build 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,4 @@ import { Tabs } from "@/app/feed/components/Tabs";
 
 import RecentTweets from './feed/recent/page';
 
-
-const Home: NextPage = async ({}) => {
-
-
-    // redirect('/feed/recent');
-
-
-
-    return (
-        <>
-            <header className="sticky top-o z-10 border-b bg-white pt-2">
-                <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
-
-                <Tabs
-                    current="Recent"
-                />
-
-            </header>
-
-            <NewTweetForm/>
-        </>
-    );
-};
-
-// export default Home;
 export default RecentTweets;
