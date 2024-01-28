9bee10d43b2362ac7025b1d7e31a21963afc306a
Color-Kat <katik7979@gmail.com>
2024-01-28 22:56:08 +0300

    profile router, generate metadata and page profile cache

--- src/app/profiles/[id]/page.tsx
+++ src/app/profiles/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata, NextPage, ResolvingMetadata } from 'next';
 import { serverClient } from "@/trpc/serverClient";
+import { cache } from "react";
 
 export const dynamic = "force-static";
 
@@ -10,29 +11,36 @@ interface Props {
     searchParams: string
 }
 
+const getProfile = cache(async (id: string) => {
+    return await serverClient.profile.getById({
+        id
+    });
+});
+
 export async function generateMetadata(
     { params, searchParams }: Props,
     parent: ResolvingMetadata
 ): Promise<Metadata> {
-    // read route params
-    const id = params.id
+    const profile = await getProfile(params.id);
 
-    const user = {
-        name: 'TODO'
+    if(!profile) return {
+        title: 'Profile Not Found'
     }
 
     return {
-        title: user.name,
+        title: profile.name,
+        description: `Awesome profile of cool person - ${profile.name}`
     }
 }
 
 const ProfilePage: NextPage<Props> = async ({params}) => {
-    const tweets = await serverClient.tweet.infiniteFeed({});
-    console.log(tweets)
+    const profile = await getProfile(params.id);
+
+    console.log(profile)
 
     return (
         <>
-wqeqwew
+            {profile?.name}
         </>
     );
 };
